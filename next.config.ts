6d349807a96da515c0fadc62ed74b22a96fbfa1d
PhucLH <phuclh@rikkeisoft.com>
2025-04-11 11:44:23 +0700

    add config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,21 @@
-import nextPwa from "next-pwa";
+import nextPwa from "next-pwa"
 
 const withPWA = nextPwa({
   dest: "public",
   register: true,
   skipWaiting: true,
-});
+})
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
+  webpack: (config: any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": require("path").resolve(__dirname, "src"),
+    }
+    return config
+  },
+}
 
-export default withPWA(nextConfig);
+export default withPWA(nextConfig)
